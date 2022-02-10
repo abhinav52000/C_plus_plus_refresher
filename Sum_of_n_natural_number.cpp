b77f94b2beb6165d617a0ea900f43217b38ff93a
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-02-11 00:38:26 +0530

    Sum of n natural number

---
+++ Sum_of_n_natural_number.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of n to which you need to know the sum of natural number \t";
+    cin>>(n);
+    cout<<"\n\nSum of n natural number is \t"<<(n*(n+1))/2;
+    return 0;
+}
