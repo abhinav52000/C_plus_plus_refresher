1cd8ce26fbd9156b3f3425d1e4069e4eda6848ed
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-02-17 20:00:49 +0530

    Last leap year program  had a problem this is correct program to check leap year

--- 23_leap_year.cpp
+++ 23_leap_year.cpp
@@ -5,14 +5,27 @@ int main()
     cout << "\nEnter the year ";
     int year;
     cin >> year;
-    if (year % 400 == 0)
+    if (year % 4 == 0)
     {
-        cout << "\nThis year " << year << " is a leap year.\n";
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                cout << "\nThis year " << year << " is a leap year.\n";
+            }
+            else
+            {
+                cout << "\nThis year " << year << " is not a leap year.\n";
+            }
+        }
+        else
+        {
+            cout << "\nThis year " << year << " is a leap year.\n";
+        }
     }
     else
     {
         cout << "\nThis year " << year << " is not a leap year.\n";
     }
-    
     return 0;
 }
