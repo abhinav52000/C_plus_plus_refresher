f9aee1eaadab425be8df639a888a8c5208c718fb
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-02-19 21:38:25 +0530

    Patttern of star using three for loop

---
+++ 41_star_pattern_print_new_type.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count = 1;
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 3-i; j++)
+        {
+            cout << "  ";
+        }
+        for (int k = 0; k < 1+i; k++)
+        {
+            cout<<" *";
+        }
+        
+        cout << endl;
+    }
+    return 0;
+}
