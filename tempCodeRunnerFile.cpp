45428edde8ff208c846c998651b6f3ed165ddcda
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-02-19 16:19:29 +0530

    Sum of all elements in an array and also use of for each loop

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[5], sum = 0;
+    cout << "\nEnter elements one after another: ";
+    for (int x : A)
+    {
+        cin >> x;
+        sum += x;
+    }
+    cout << "\nSum of each element in this array is " << sum << endl;
+    return 0;
+}
