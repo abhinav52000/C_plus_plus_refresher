3ed9cd402739d1abd4f8181de8b9665ec8e2859d
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-02-19 16:24:13 +0530

    Max of all elemetns in the given array

---
+++ 36_max_of_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[5], max=INT32_MIN;
+    cout << "\nEnter elements one after another: ";
+    for (int x : A)
+    {
+        cin >> x;
+        if(max<x)
+        {
+            max=x;
+        }
+    }
+    cout << "\nMaximum number among all in this array is " << max;
+    return 0;
+}
