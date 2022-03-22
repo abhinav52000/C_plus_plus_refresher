1692d5a244bb2b9b68d2b43e527e4c20ad9be361
Abhinav kumar <abhinhavkumar2000@gmail.com>
2022-03-22 21:56:12 +0530

    Input output stream operator overloading

---
+++ 69_input_output_stream_operator_overloading.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+class Complex
+{
+private:
+    int real;
+    int img;
+
+public:
+    Complex(int r = 0, int i = 0)
+    {
+        real = r;
+        img = i;
+    }
+    friend ostream &operator<<(ostream &out, Complex &c);
+};
+ostream &operator<<(ostream &out, Complex &c)
+{
+    out << c.real << " + " << c.img << "i" << endl;
+    return out;
+}
+int main()
+{
+    Complex c(10, 5);
+    // cout << c << endl;
+    operator<<(cout, c); // both of the statement work in same way.
+    return 0;
+}
